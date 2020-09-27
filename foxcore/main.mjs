d329c588a9d4aaf79b9ef97c54b4edcf0ff6c9d2
imoegirl <imoegirl@outlook.com>
2020-09-27 22:00:57 +0800

    Init project

--- foxcore/main.mjs
+++ foxcore/main.mjs
@@ -1,4 +1,1 @@
-import Log from "./log/log.mjs";
-
-const logger = new Log("./Logs/Main");
 
---
+++ foxcore/mod.js
@@ -0,0 +1,6 @@
+import Log from "./log/log.mjs";
+const logger = new Log("./Logs/Main");
+
+export {logger};
+
+export default null;
